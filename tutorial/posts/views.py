43db94c7251c58f2a48294ae7cb3ac7465ccc0c7
YaroslavKazeev <yaroslav.kazeev@outlook.com>
2023-11-04 10:14:16 +0100

    Initial commit

---
+++ tutorial/posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from .models import Post
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'post_list' : posts
+    }
+    return render(request, "posts/post_list.html", context)
